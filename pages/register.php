c2d7ca6797571bf5fd935062dd26fdfc85122576
Rick <Rslierendregt@roc-dev.com>
2023-05-11 11:02:06 +0200

    added content to register page

--- pages/register.php
+++ pages/register.php
@@ -1,4 +1,20 @@
+<!DOCTYPE html>
 <html>
 
+    <head>
+        <title>Registration</title>
+    </head>
+
+    <body>
+        <h2>Register</h2>
+        <form method="post" action="register.php">
+            <input type="text" name="first name" required>
+            <input type="text" name="last name" required>
+            <input type="email" name="email" required>
+            <input type="password" name="password" required>
+            <button type="submit" class="btn" name="reg_user">Register</button>
+            <p>Already a member? Log in here</p>
+        </form>
+    </body>
 
 </html>

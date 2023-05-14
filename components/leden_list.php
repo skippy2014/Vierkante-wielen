9f4e61f6c522e662d0ae16f14eeb1541d2e835c4
TRWcode <TRWcode@users.noreply.github.com>
2023-05-15 00:48:57 +0200

    homepages instrcteurs/leden + lijst leden/

---
+++ components/leden_list.php
@@ -0,0 +1,26 @@
+<table>
+    <thead>
+      <tr>
+        <th>Naam Lid</th>
+        <th>E-mail</th>
+        <th>Telefoon Nummer</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Remco Boeie</td>
+        <td>123@mail</td>
+        <td>06-1234567</td>
+      </tr>
+      <tr>
+        <td>Bart Smit</td>
+        <td>321@mail</td>
+        <td>06-3213123</td>
+      </tr>
+      <tr>
+        <td>Mark Bal</td>
+        <td>222@mail</td>
+        <td>06-231313</td>
+      </tr>
+    </tbody>
+  </table>

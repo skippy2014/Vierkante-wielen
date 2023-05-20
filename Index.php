2fd7b5e3e9ee85dab1f7c324ab57d0c0b414187d
Rick <Rslierendregt@roc-dev.com>
2023-05-20 14:48:39 +0200

    Changed location of css file

--- Index.php
+++ Index.php
@@ -1,65 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Home Vierkante wielen</title>
-   <link rel="STYLESHEET" type="text/css"  href="../css/style.css">
-</head>
-<body>
-  <!-- <div class="header"><h1>Header here</h1></div> -->
-  <div class="headerImage">
 
- <div class="info-container1"> <h2>Rijschool de Vierkante wielen</h2>
-  <br>
-    <p>Klaar om de weg op te gaan? <br> Rijschool Vierkante wielen helpt je bij het behalen van je rijbewijs.<br>
-  Wij bieden flexibele lessen en ervaren instructeurs <br> om je te helpen vertrouwen te krijgen achter het stuur 
-  en klaar te zijn voor de weg. <br>  Meld je vandaag nog aan!
-  </p>
-  
-<button>Schrijf je in!</button>
-</div></div></div>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Vierkante wielen</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
 
-<div class="info-rijschool"> <h2 style="padding-top:10px;"> Informatie rijschool</h2>
-  <br>
-<p>Onze rijschool is gericht op klanten met een fysieke beperking <br> en we zijn er trots op dat we onze
-   studenten kunnen helpen om snel en zelfverzekerd de weg op te gaan. <br>
-   We begrijpen de uitdagingen die gepaard gaan met het leren rijden met een fysieke beperking<br> en we
-    zijn er om onze studenten te ondersteunen en te begeleiden op hun reis naar het behalen van hun rijbewijs. <br>
-    Onze lessen zijn rustig en doelgericht,<br> en of je nu net begint of al wat ervaring hebt, we hebben lessen
-    die aansluiten op jou behoeften.<br>  zodat onze studenten zich op hun gemak voelen en hun volledige aandacht
-    kunnen richten op het leren rijden.  </p>
-</div>
+  <body>
+    <!-- <div class="header"><h1>Header here</h1></div> -->
+    <div class="headerImage">
+
+      <div class="info-container1">
+        <h2>Rijschool de Vierkante wielen</h2>
+        <br>
+        <p>Klaar om de weg op te gaan? <br> Rijschool Vierkante wielen helpt je bij het behalen van je rijbewijs.<br>
+          Wij bieden flexibele lessen en ervaren instructeurs <br> om je te helpen vertrouwen te krijgen achter het
+          stuur
+          en klaar te zijn voor de weg. <br> Meld je vandaag nog aan!
+        </p>
+
+        <button>Schrijf je in!</button>
+      </div>
+    </div>
+    </div>
+
+    <div class="info-rijschool">
+      <h2 style="padding-top:10px;"> Informatie rijschool</h2>
+      <br>
+      <p>Onze rijschool is gericht op klanten met een fysieke beperking <br> en we zijn er trots op dat we onze
+        studenten kunnen helpen om snel en zelfverzekerd de weg op te gaan. <br>
+        We begrijpen de uitdagingen die gepaard gaan met het leren rijden met een fysieke beperking<br> en we
+        zijn er om onze studenten te ondersteunen en te begeleiden op hun reis naar het behalen van hun rijbewijs. <br>
+        Onze lessen zijn rustig en doelgericht,<br> en of je nu net begint of al wat ervaring hebt, we hebben lessen
+        die aansluiten op jou behoeften.<br> zodat onze studenten zich op hun gemak voelen en hun volledige aandacht
+        kunnen richten op het leren rijden. </p>
+    </div>
 
 
 
 
-  
-  <div class="registration"> <h2> Waarom rijschool Vierkante wielen?</h2>
-<ul> 
-  <li>De beste instructeurs</li>
-  <li>Les in een automaat of schakelauto</li>
-  <li>Extra begeleiding voor mensen met een fysieke beperking</li>
-</ul>
-    
-<div class="Over-ons"><h2>Over ons</h2>
-<p>Het is onze passie om mensen te helpen bij het leren rijden. We zijn trots op onze duidelijke uitleg,
-   professionele begeleiding en de steun die we kunnen geven aan mensen met een fysieke beperking,
-   waarmee we onze studenten in staat stellen om snel en zelfverzekerd
-de weg op te gaan. 
-We willen onze studenten niet alleen helpen om hun rijbewijs te halen, maar ook om hun 
-onafhankelijkheid en vrijheid te vergroten. We zijn er trots op dat we een verschil kunnen maken in het leven 
-van anderen.</p> </div>
 
-<div class="tarieven"><h2> tarieven</h2></div>
+    <div class="registration">
+      <h2> Waarom rijschool Vierkante wielen?</h2>
+      <ul>
+        <li>De beste instructeurs</li>
+        <li>Les in een automaat of schakelauto</li>
+        <li>Extra begeleiding voor mensen met een fysieke beperking</li>
+      </ul>
 
-<div class="Info-Rijschoolhouder"><h2>Informatie Rijschoolhouder</h2></div>
+      <div class="Over-ons">
+        <h2>Over ons</h2>
+        <p>Het is onze passie om mensen te helpen bij het leren rijden. We zijn trots op onze duidelijke uitleg,
+          professionele begeleiding en de steun die we kunnen geven aan mensen met een fysieke beperking,
+          waarmee we onze studenten in staat stellen om snel en zelfverzekerd
+          de weg op te gaan.
+          We willen onze studenten niet alleen helpen om hun rijbewijs te halen, maar ook om hun
+          onafhankelijkheid en vrijheid te vergroten. We zijn er trots op dat we een verschil kunnen maken in het leven
+          van anderen.</p>
+      </div>
 
-<div class="Call-to-action"><h2>Schrijf je nu in!</h2></div>
+      <div class="tarieven">
+        <h2> tarieven</h2>
+      </div>
 
-  
-  
+      <div class="Info-Rijschoolhouder">
+        <h2>Informatie Rijschoolhouder</h2>
+      </div>
 
-</body>
+      <div class="Call-to-action">
+        <h2>Schrijf je nu in!</h2>
+      </div>
+
+
+
+
+  </body>
+
 </html>

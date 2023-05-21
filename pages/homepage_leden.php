a3af1c14446cafa52a97d9a4430854edc62cd35c
TRWcode <thomas_sibbald2@hotmail.com>
2023-05-21 17:21:12 +0200

    branch aangemaakt + notitie wt op pagian moet

--- pages/homepage_leden.php
+++ pages/homepage_leden.php
@@ -11,7 +11,16 @@
 
 <?php include '../components/header.php'; ?>
 
+<h3>info klantnaam met counter van les tegoed </h3>
+
 <h3>Welkom klantnaam </h3>
+
+<h3>meldingen lijst komt hier </h3>
+
+<h3>lijst van ingeplande datums + annuleer en wijzig functie</h3>
+
+<h3>examen info  </h3>
+
 
 </body>
 </html>

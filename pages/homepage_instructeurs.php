f83dbc7a4e7720e1706bfe587e8bcbe59d813c79
Pjotr <pdevos@roc-dev.com>
2023-05-22 11:11:41 +0200

    Overzichtpagina
    Ziekmelden/nieuwe les inplannen & Profiel pagina met basis css

---
+++ pages/homepage_instructeurs.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Vierkanten Wielen</title>
+</head>
+<style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .profile-header {
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+        }
+
+        .profile-picture {
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+            background-color: #ccc;
+            margin-right: 20px;
+        }
+
+        .profile-info {
+            flex: 1;
+        }
+
+        .profile-info h1 {
+            font-size: 24px;
+            margin: 0;
+        }
+
+        .profile-info p {
+            margin: 0;
+            color: #888;
+        }
+
+        .profile-info a {
+            color: #333;
+            text-decoration: none;
+        }
+
+        .profile-about {
+            margin-bottom: 20px;
+        }
+
+        .profile-about h2 {
+            font-size: 18px;
+            margin-bottom: 10px;
+        }
+
+        .profile-about p {
+            margin: 0;
+            color: #555;
+        }
+    </style>
+<body>
+<?php include '../components/header.php'; ?>
+
+<h2>Welkom Instructeurs</h2>
+
+<?php 
+echo "Vandaag is het " . date("l");
+?>
+<br>
+    <div class="container">
+        <div class="profile-header">
+            <div class="profile-picture"></div>
+            <div class="profile-info">
+                <h1>Naam</h1>
+                <p>Email: </p>
+                <p>Leeftijd: 30</p>
+            </div>
+        </div>
+
+        <div class="profile-about">
+            <h2>Over</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere bibendum elit, et semper elit aliquam eu. Fusce tincidunt fermentum velit, a consequat lacus bibendum in. Donec eleifend, velit id efficitur commodo, enim ligula egestas sem, at iaculis nisi nunc et ex.</p>
+        </div>
+
+
+    </div>
+
+<a href="ziekmelden.php"><button>Ziekmelden</button></a><br>
+<a href="nieuwe_les_inplannen.php"><button>Nieuwe les inplannen</button></a>
+
+
+
+
+</body>
+</html>
---
+++ pages/nieuwe_les_inplannen.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Vierkanten Wielen</title>
+    </head>
+    <body>
+    <?php include '../components/header.php'; ?>
+
+
+    <h2>Nieuwe les inplannen</h2>
+    <form method="POST" action="">
+        <label for="naam">Naam *:</label>
+        <input type="text" id="naam" name="naam" required><br><br>
+
+        <label for="email">Email *:</label>
+        <input type="text" id="email" name="email" required><br><br>
+
+        <label for="Telefoon">Telefoon *:</label>
+        <input type="text" id="Telefoon" name="Telefoon" required><br><br>
+
+        <label for="Contact_Opnemen">Telefonisch contact gewenst? :</label>
+        <input type="radio" id="Ja" name="Contact_Opnemen" value="Ja">
+        <label for="Ja">Ja</label>
+        
+        <input type="radio" id="Nee" name="Contact_Opnemen" value="Nee">
+        <label for="Nee">Nee</label><br><br>
+
+        <label for="Welke_Dag">Welke dag? *:</label>
+        <input type="date" id="Welke_Dag" name="Welke_Dag" required><br><br>
+        
+        
+        <input type="submit" value="Verstuur">
+    
+
+
+
+    </form>
+
+
+
+    </body>
+    </html>

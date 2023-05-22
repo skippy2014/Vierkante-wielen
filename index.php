961e2ec07dd760b35635c46c8aea3a754a69c44e
TRWcode <thomas_sibbald2@hotmail.com>
2023-05-22 12:41:04 +0200

    update main

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home Vierkante wielen</title>
+   <link rel="STYLESHEET" type="text/css"  href="css/style.css">
+</head>
+<body>
+  <!-- <div class="header"><h1>Header here</h1></div> -->
+  <div class="headerImage">
+
+ <div class="info-container1"> <h2>Rijschool de Vierkante wielen</h2>
+  <br>
+    <p>Klaar om de weg op te gaan? <br> Rijschool Vierkante wielen helpt je bij het behalen van je rijbewijs.<br>
+  Wij bieden flexibele lessen en ervaren instructeurs <br> om je te helpen vertrouwen te krijgen achter het stuur 
+  en klaar te zijn voor de weg. <br>  Meld je vandaag nog aan!
+  </p>
+  <a href="pages/register.php">
+  <button>Schrijf je in!</button></a>
+</div></div></div>
+
+<div class="info-rijschool"> <h2 style="padding-top:25px;"> Informatie rijschool</h2>
+  <br>
+<p>Onze rijschool is gericht op klanten met een fysieke beperking <br> en we zijn er trots op dat we onze
+   studenten kunnen helpen om snel en zelfverzekerd de weg op te gaan. <br>
+   We begrijpen de uitdagingen die gepaard gaan met het leren rijden met een fysieke beperking<br> en we
+    zijn er om onze studenten te ondersteunen en te begeleiden op hun reis naar het behalen van hun rijbewijs. <br>
+    Onze lessen zijn rustig en doelgericht,<br> en of je nu net begint of al wat ervaring hebt, we hebben lessen
+    die aansluiten op jou behoeften.<br>  zodat onze studenten zich op hun gemak voelen en hun volledige aandacht
+    kunnen richten op het leren rijden.  </p>
+</div>
+
+  <div class="registration">
+   <div class="regItem1"><img src="img/Bestuurder.png" alt="Bestuurder"></div>  
+   <div class="regItem2"><h2> Waarom is autorijschool Vierkante wielen <br> de beste keuze?</h2></div>
+  <div class="regItem3">
+    <ul class="pros"> 
+  <li>We hebben de beste instructeurs</li>
+  <li>Lessen in een automaat of schakelauto</li>
+  <li>Extra begeleiding voor mensen met een fysieke beperking</li>
+  <li>Rustig en doelgericht</li>
+</ul></div></div>
+    
+<div class="Over-ons">
+<div class="onsD1"><img src="img/ManAchterStuur.png" alt="Man achter stuur"></div>
+  <div class="onsD2"><h2>Over ons</h2>
+<p>Het is onze passie om mensen te helpen bij het leren rijden. We zijn trots op onze duidelijke uitleg,
+   professionele begeleiding <br> en de steun die we kunnen geven aan mensen met een fysieke beperking,
+   waarmee we onze studenten in staat stellen<br> om snel en zelfverzekerd
+de weg op te gaan.</p></div>
+<div class="onsD3"><p>
+We willen onze studenten niet alleen helpen om hun rijbewijs te halen,<br> maar ook om hun 
+onafhankelijkheid en vrijheid te vergroten.<br> We zijn er trots op dat we een verschil kunnen maken in het leven 
+van anderen.</p></div></div>
+
+<div class="tarieven">
+  <div class="pakket0"><h2> Tarieven</h2></div>
+<div class="pakket1"> Instap pakket 1 <br>      € 2004,00      </div>
+<div class="pakket2"> Instap pakket 2 <br>     € 2644,00      </div>
+<div class="pakket3"> Instap pakket 3 <br>    € 3064,00       </div>
+<div class="pakketD1"> 
+  <ul>
+   <li>30 lesuren (praktijk)</li>
+  <li>Theorie-examen</li>
+  <li>Tussentijdse toets</li>
+    <li> CBR-praktijkexamen</li>
+    <li>Terugbetaling van ongebruikte lessen</li>
+</ul>
+<a href="pages/register.php">
+  <button>Ik wil dit pakket!</button></a>
+ </div>
+<div class="pakketD2">  
+  <ul>
+    <li>40 lesuren (praktijk)</li>
+   <li>Theorie-examen </li>
+   <li>Tussentijdse toets</li>
+     <li> CBR-praktijkexamen</li>
+  <li>Terugbetaling van ongebruikte lessen</li>
+ </ul>
+ <a href="pages/register.php">
+  <button>Ik wil dit pakket!</button></a>
+</div>
+<div class="pakketD3">   
+  <ul>
+    <li>50 lesuren (praktijk)</li>
+   <li>Theorie-examen </li>
+   <li>Tussentijdse toets</li>
+     <li> CBR-praktijkexamen</li>
+     <li>Terugbetaling van ongebruikte lessen</li>
+ </ul> 
+ <a href="pages/register.php">
+  <button>Ik wil dit pakket!</button></a>
+</div>
+</div>
+
+
+<div class="Info-Rijschoolhouder">
+  <div class="infoR1"><img src="img/Instructeur.png" alt="Bestuurder"></div>
+  <div class="infoR2"><h2>Informatie Rijschoolhouder</h2></div>
+<div class="infoR3"><p> Onze rijschoolhouder is een ervaren en professionele instructeur die zijn leerlingen 
+  op een vriendelijke en ondersteunende manier begeleidt. Hij is gepassioneerd over het helpen van leerlingen
+   om hun rijvaardigheid te verbeteren en hen te ondersteunen bij het behalen van hun rijbewijs. </p></div>
+
+</div>
+
+<div class="Call-to-action">
+  <div class="callA1"><img src="img/auto.png" alt="Auto"></div>
+ <div class="callA2"><h2>Schrijf je nu in!</h2></div> 
+ <div class="callA3"><p>Kies onze rijschool voor professionele instructeurs, betaalbare lessen en een veilige
+   en leuke leeromgeving. We helpen je om veilig en vol vertrouwen de weg op te gaan!</p>
+   <a href="pages/register.php">
+   <button>Schrijf je in!</button></a>
+  </div>
+   
+   </div>
+</body>
+</html>

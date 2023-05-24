0f70769881a6286870bf563b88faf24f23209725
Oskar Dubbelink <Oskardubbelink@gmail.com>
2023-05-24 11:48:11 +0200

    Dropdown menu van de pakketten toegevoegd

--- pages/register.php
+++ pages/register.php
@@ -17,6 +17,13 @@
             <br>
             <input type="password" name="password" placeholder="Wachtwoord" required>
             <br>
+            <label for="pakket">Kies een pakket</label>
+           <select name="pakket" id="pakket">
+    	    <option value="Pakket1">Instap pakket 1</option>
+            <option value="Pakket2">Instap pakket 2</option>
+            <option value="Pakket3">Instap pakket 3</option>
+        </select>
+            <br>
             <button type="submit" class="btn" name="reg_user">Register</button>
             <p>Already a member? Log in
                 <a href="loginpage.php">here</a>

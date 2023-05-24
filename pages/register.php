c60860f8299cb27ab890b6bf71a4032e57a67c17
Rick <Rslierendregt@roc-dev.com>
2023-05-24 11:57:31 +0200

    added left side of register page

--- pages/register.php
+++ pages/register.php
@@ -7,21 +7,25 @@
     </head>
 
     <body>
-        <h2>Register</h2>
-        <form method="post" action="post_register_lid.php" class="form">
-            <input type="text" name="first name" placeholder="Voornaam" required class="input_field">
-            <br>
-            <input type="text" name="last name" placeholder="Achternaam" required>
-            <br>
-            <input type="email" name="email" placeholder="Email" required>
-            <br>
-            <input type="password" name="password" placeholder="Wachtwoord" required>
-            <br>
-            <button type="submit" class="btn" name="reg_user">Register</button>
-            <p>Already a member? Log in
-                <a href="loginpage.php">here</a>
-            </p>
-        </form>
+
+        <div class="register_grid">
+            <form method="post" action="post_register_lid.php" class="form_register">
+                <h2>Register</h2>
+                <input type="text" name="first name" placeholder="Voornaam" required class="input_field">
+                <input type="text" name="last name" placeholder="Achternaam" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Wachtwoord" required>
+                <button type="submit" class="btn" name="reg_user">Register</button>
+                <p>Already a member? Log in
+                    <a href="loginpage.php">here</a>
+                </p>
+            </form>
+            <div>
+                <p>text</p>
+                <!-- LIJST MET TARIEVEN -->
+            </div>
+        </div>
+
     </body>
 
 </html>

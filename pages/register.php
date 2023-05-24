2f78e833a35fd621586b56f7fb943e6bc262aaff
Rick <Rslierendregt@roc-dev.com>
2023-05-24 12:09:53 +0200

    dropdown pakketten toegevoegd aan register pagina
    van de register leerling branch

--- pages/register.php
+++ pages/register.php
@@ -15,6 +15,12 @@
                 <input type="text" name="last name" placeholder="Achternaam" required>
                 <input type="email" name="email" placeholder="Email" required>
                 <input type="password" name="password" placeholder="Wachtwoord" required>
+                <select name="pakket" id="pakket">
+                    <option value="Default" disabled selected>Kies een instappakket</option>
+                    <option value="Pakket1">Instap pakket 1</option>
+                    <option value="Pakket2">Instap pakket 2</option>
+                    <option value="Pakket3">Instap pakket 3</option>
+                </select>
                 <button type="submit" class="btn" name="reg_user">Register</button>
                 <p>Already a member? Log in
                     <a href="loginpage.php">here</a>

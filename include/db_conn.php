b353cf5ece77ecf382d34b07a66c639286d0dac9
TRWcode <thomas_sibbald2@hotmail.com>
2023-06-01 12:07:40 +0200

    database standaard

--- include/db_conn.php
+++ include/db_conn.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tt";
+$dbname = "vierkantewielen";
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);

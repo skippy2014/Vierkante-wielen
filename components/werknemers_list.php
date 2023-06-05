13fdb0d7dfd25473e673aa68a154eced439a8fbb
TRWcode <TRWcode@users.noreply.github.com>
2023-06-06 00:07:56 +0200

    beide lijsten werken lokaal. nu nog voor iedereen

--- components/werknemers_list.php
+++ components/werknemers_list.php
@@ -1,24 +1,43 @@
 <?php
 require_once "../include/db_conn.php";
 
-$sql = "SELECT * FROM users";
-    $result = $conn->query($sql);
 
+$sql = "SELECT gebruiker.id_gebruiker, gebruiker.voornaam, gebruiker.achternaam, gebruiker.email, intructeur.id_intructeur, intructeur.admin, intructeur.telefoon
+        FROM gebruiker
+        JOIN intructeur ON gebruiker.id_gebruiker = intructeur.id_intructeur";
+
+$result = $conn->query($sql);
+
+
+if ($result) {
+    
     if ($result->num_rows > 0) {
+        
         echo "<table>";
-        echo "<tr><th>ID</th><th>Name</th><th>Email</th></tr>";
-
+        echo "<tr><th>ID intructeur</th><th>Voornaam</th><th>Achternaam</th><th>E-mail</th><th>Telefoon</th><th>Admin</th></tr>";
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
-            echo "<td>" . $row["id"] . "</td>";
-            echo "<td>" . $row["name"] . "</td>";
+            echo "<td>" . $row["id_intructeur"] . "</td>";
+            echo "<td>" . $row["voornaam"] . "</td>";
+            echo "<td>" . $row["achternaam"] . "</td>";
             echo "<td>" . $row["email"] . "</td>";
+            echo "<td>" . $row["telefoon"] . "</td>";
+
+            if ($row["admin"] == 1) {
+        
+            echo "<td>ja</td>";
+            } else {
+                echo "<td>nee</td>";
+            }
             echo "</tr>";
         }
-
         echo "</table>";
     } else {
         echo "No results found.";
     }
-    $conn->close();
-    ?> 
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>

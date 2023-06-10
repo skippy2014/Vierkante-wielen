9fa42f190658622b2eb40b3851cc2ee5d0914747
TRWcode <thomas_sibbald2@hotmail.com>
2023-06-10 20:17:18 +0200

    login page opgeschoont

--- pages/loginpage.php
+++ pages/loginpage.php
@@ -1,16 +1,5 @@
 <?php
-session_start();
-
-$dbHost = 'localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'vierkantewielendemo';
-
-$connection = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
+include '../include/db_conn.php';
 
 if (isset($_GET["loguit"])) {
     $_SESSION = array();
@@ -49,16 +38,9 @@ if (isset($_POST['login_button'])) {
                 header('Location: ../pages/account_settings.php');
             } else {
                 header('Location: ../pages/select_lespakket.php');
-            }
-           // $message = "Welkom!";
-        } else {
-            $message = "Foutieve login gegevens";
+            }      
         }
-    } else {
-        $message = "Foutieve login gegevens";
     }
-} else {
-    $message = "Login";
 }
 ?>
 
@@ -73,31 +55,14 @@ if (isset($_POST['login_button'])) {
         <div class="general_layout">
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form_login">
                 <h2 class="TOPh1_login_page">
-                    <?php echo $message; ?>
+                    Login
                 </h2>
                 <input type="email" name="email" placeholder="Email" required>
                 <input type="password" name="password" placeholder="Password" required>
                 <br>
                 <button type="submit" class="btn" name="login_button">Log in</button>
-
                 <p>Nog geen account? Maak er <a href="register.php">hier</a> een aan</p>
                 <br><br>
-
-                <p><a href="../index.php">Website</a></p>
-                <p><a href="loginpage.php?loguit">Uitloggen</a></p>
-
-                <?php
-                if (isset($_SESSION['gebruiker']) && ($_SESSION['gebruiker']['rol'] === 'instructeur' || $_SESSION['gebruiker']['rol'] === 'eigenaar')) {
-                    echo '<p><a href="homepage_instructeurs.php">Instructeur</a></p>';
-                }
-                ?>
-                <?php
-                if (isset($_SESSION['gebruiker']) && $_SESSION['gebruiker']['rol'] === 'eigenaar') {
-                    echo '<p><a href="homepage_admin.php">Eigenaar</a></p>';
-                }
-                ?>
-
-
             </form>
 
         </div>

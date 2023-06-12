959f8fe0b8677d33845c3656defb0a16d69aebb3
Oskar Dubbelink <Oskardubbelink@gmail.com>
2023-06-12 11:48:19 +0200

    form

--- pages/ziekmelden.php
+++ pages/ziekmelden.php
@@ -13,10 +13,13 @@
     <body>
 
 
-    <h2>Ziekmeldings formulier</h2>
+    <h2>Afmeld formulier</h2>
     <form method="POST" action="">
         <label for="naam">Naam *:</label>
         <input type="text" id="naam" name="naam" required><br><br>
+
+        <label for="Welke_Dag"> E-mail *:</label>
+        <input type="email" id="Welke_Dag" name="Welke_Dag" required><br><br>
 
         <label for="reden_afwezig">Reden afwezig *:</label>
         <input type="radio" id="Ziek" name="Reden_Ziek" value="Ziek">
@@ -25,23 +28,8 @@
         <input type="radio" id="Andere_Reden" name="Reden_Ziek" value="Andere_Reden">
         <label for="Reden_Ziek">Andere reden</label><br><br>
 
-        <label for="Telefoon">Telefoon *:</label>
-        <input type="text" id="Telefoon" name="Telefoon" required><br><br>
-
-        <label for="Contact_Opnemen">Telefonisch contact gewenst? :</label>
-        <input type="radio" id="Ja" name="Contact_Opnemen" value="Ja">
-        <label for="Ja">Ja</label>
-        
-        <input type="radio" id="Nee" name="Contact_Opnemen" value="Nee">
-        <label for="Nee">Nee</label><br><br>
-
         <label for="Welke_Dag">Welke dag? *:</label>
         <input type="date" id="Welke_Dag" name="Welke_Dag" required><br><br>
-        
-        <label for="Welke_Dag_Terug">Wanneer weer terug? *:</label>
-        <input type="date" id="Welke_Dag_Terug" name="Welke_Dag_Terug" required><br><br>
-
-
 
         <input type="submit" value="Verstuur">
     

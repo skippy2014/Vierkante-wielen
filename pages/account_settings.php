7afa6f1b2583ec03d572f3ad4bbe690cbd57a230
Rick <Rslierendregt@roc-dev.com>
2023-06-12 11:23:59 +0200

    Remove scrollbar hiding style from account settings page
    The commit removes the CSS style that hides scrollbars from the account settings page. This change will allow users to scroll through the content on the page using their mouse or touchpad.

--- pages/account_settings.php
+++ pages/account_settings.php
@@ -4,12 +4,6 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/style.css">
-    <style>
-      /* Hide scrollbars */
-      body {
-        overflow: hidden;
-      }
-    </style>
   </head>
 
   <?php

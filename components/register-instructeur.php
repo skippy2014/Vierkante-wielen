7fc270934e841ca193329c4688d13cff9793dcb4
Rick <Rslierendregt@roc-dev.com>
2023-06-12 21:04:43 +0200

    Refactor phone number input field and add script to format Dutch phone numbers
    The code changes refactor the phone number input field in the registration form to accept any valid phone number, and adds a script that formats the entered phone number into the Dutch format. The new script listens for user input on the phone number field and applies formatting rules based on the length of the entered digits.

--- components/register-instructeur.php
+++ components/register-instructeur.php
@@ -1,9 +1,8 @@
 <form method="post" action="../include/post_register-instructeur.php">
   <input type="text" name="voornaam" placeholder="Voornaam" required><br>
-  <input type="text" name="achternaam" placeholder="Achternaam" required><br>  
+  <input type="text" name="achternaam" placeholder="Achternaam" required><br>
   <input type="email" name="email" placeholder="Email" required><br>
-  <input type="tel" id="phone" name="telefoon" placeholder="06-12345678" pattern="[06]{2}-[0-9]{8}" required><br>
-  <label for="telefoon">telefoon:(06-12345678)</label><br>
+  <input type="tel" id="phoneInput" name="telefoon" placeholder="Telefoonnummer" maxlength="11" required><br>
   <input type="password" name="password" placeholder="Wachtwoord" required><br>
 
   <select name="rol" required>
@@ -12,5 +11,30 @@
     <option value="eigenaar">Eigenaar</option>
   </select><br>
 
- <button type="submit" class="btn">Register</button>
+  <button type="submit" class="btn">Register</button>
 </form>
+
+<script>
+  const phoneInput = document.getElementById('phoneInput');
+
+  phoneInput.addEventListener('input', function () {
+    formatPhoneNumber();
+  });
+
+  function formatPhoneNumber() {
+    let phoneNumber = phoneInput.value.replace(/\D/g, ''); // Remove non-digit characters
+
+    // Apply the Dutch phone number format
+    if (phoneNumber.length > 2 && phoneNumber.length <= 5) {
+      phoneNumber = phoneNumber.replace(/(\d{2})(\d+)/, '$1-$2');
+    } else if (phoneNumber.length > 5 && phoneNumber.length <= 10) {
+      phoneNumber = phoneNumber.replace(/(\d{2})(\d{3})(\d+)/, '$1-$2$3');
+    } else if (phoneNumber.length > 10) {
+      phoneNumber = phoneNumber.replace(/(\d{2})(\d{3})(\d{2})(\d+)/, '$1-$2$3-$4');
+    }
+
+    phoneInput.value = phoneNumber;
+  }
+
+
+</script>

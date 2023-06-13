dd9f37f8aca6222b5ae2f42747b553d370eab13a
Rick <Rslierendregt@roc-dev.com>
2023-06-13 10:43:58 +0200

    Refactor tab buttons and add new functionality
    - Refactored the tab buttons to be generated dynamically using an array
    - Added a new feature that initializes the page by opening the first tab or the tab specified in the hash
    - Added event listeners for each button to update URL hash when clicked
    - Updated script to scroll to top of page and remove overflow property on load

--- pages/account_settings.php
+++ pages/account_settings.php
@@ -23,80 +23,94 @@
   <body>
     <div class="layout">
       <div class="tab">
-        <button class="tablinks" onclick="openTab(event, 'Overzicht')" id="Overzicht_btn">Overzichtspagina</button>
-        <button class="tablinks" onclick="openTab(event, 'Accountsettings')" id="Accountsettings_btn">Account
-          Instellingen</button>
-        <button class="tablinks" onclick="openTab(event, 'LesToevoegen')" <?php if (!$toonLesToevoegen)
-          echo 'style="display:none"'; ?> id="LesToevoegen_btn">Les toevoegen</button>
-        <button class="tablinks" onclick="openTab(event, 'Meldingen')" id="Meldingen_btn">Meldingen</button>
-        <button class="tablinks" onclick="openTab(event, 'Subscription')" id="Subscription_btn">Abonnement</button>
-        <button class="tablinks" onclick="openTab(event, 'Register')" <?php if (!$toonLesToevoegen)
-          echo 'style="display:none"'; ?> id="Register_btn">Register</button>
+        <?php
+        $tabs = [
+          ['Overzichtspagina', 'Overzicht', true],
+          ['Account Instellingen', 'Accountsettings', true],
+          ['Les toevoegen', 'LesToevoegen', $toonLesToevoegen],
+          ['Meldingen', 'Meldingen', true],
+          ['Abonnement', 'Subscription', true],
+          ['Register', 'Register', $toonLesToevoegen],
+        ];
+        foreach ($tabs as [$label, $id, $visible]) {
+          if (!$visible) {
+            continue;
+          }
+          echo '<button class="tablinks" onclick="openTab(event, \'' . $id . '\')" id="' . $id . '_btn">' . $label . '</button>';
+        }
+        ?>
         <button class="tablinks" onclick="window.location='loginpage.php?loguit'">Log Uit</button>
       </div>
       <?php include_once '../components/sidebar_links.php'; ?>
     </div>
   </body>
 
+
   <script>
     // Wait until document is fully loaded
-    document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("DOMContentLoaded", function () {
+      const body = document.body;
+      const html = document.documentElement;
       // Remove overflow property to restore scrolling
-      document.body.style.overflow = "auto";
+      body.style.overflow = "auto";
 
       // Scroll to top of page
       window.scrollTo(0, 0);
+
+      // Initialize the page by opening the first tab or the tab specified in the hash
+      const hash = window.location.hash.substring(1);
+      var elemToClick = document.getElementById("Overzicht_btn");
+
+      if (hash) {
+        elemToClick = document.querySelector(".tablinks[id='" + hash + "_btn']");
+      }
+
+      elemToClick.click();
     });
+
     function openTab(evt, tabName) {
-      var i, tabcontent, tablinks;
-      tabcontent = document.getElementsByClassName("tabcontent");
-      for (i = 0; i < tabcontent.length; i++) {
+      const tabcontent = document.getElementsByClassName("tabcontent");
+      for (let i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
       }
-      tablinks = document.getElementsByClassName("tablinks");
-      for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-      }
-      document.getElementById(tabName).style.display = "block";
-      evt.currentTarget.className += " active";
-      document.documentElement.scrollTop = 0; // Scroll to the top of the page
-    }
 
-    // Initialize the page by opening the first tab or the tab specified in the hash
-    var hash = window.location.hash.substring(1);
-    if (hash) {
-      document.querySelector(".tablinks[id='" + hash + "_btn']").click();
-    } else {
-      document.getElementById("Overzicht_btn").click();
+      const tablinks = document.getElementsByClassName("tablinks");
+      for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+      }
+
+      const elem = document.getElementById(tabName);
+      elem.style.display = "block";
+      evt.currentTarget.classList.add("active");
+      document.documentElement.scrollTop = 0; // Scroll to the top of the page
+
+      window.location.hash = tabName;
     }
 
     // Initialize an event listener for each button
-    document.getElementById("LesToevoegen_btn").addEventListener("click", function () {
-      window.location.hash = "LesToevoegen";
-    });
-    document.getElementById("Overzicht_btn").addEventListener("click", function () {
-      window.location.hash = "Overzicht";
-    });
-    document.getElementById("Accountsettings_btn").addEventListener("click", function () {
-      window.location.hash = "Accountsettings";
-    });
-    document.getElementById("Meldingen_btn").addEventListener("click", function () {
-      window.location.hash = "Meldingen";
-    });
-    document.getElementById("Subscription_btn").addEventListener("click", function () {
-      window.location.hash = "Subscription";
-    });
-    document.getElementById("Register_btn").addEventListener("click", function () {
-      window.location.hash = "Register";
+    const buttonIds = [
+      "LesToevoegen_btn",
+      "Overzicht_btn",
+      "Accountsettings_btn",
+      "Meldingen_btn",
+      "Subscription_btn",
+      "Register_btn",
+    ];
+
+    buttonIds.forEach((btnId) => {
+      document.getElementById(btnId).addEventListener("click", function () {
+        window.location.hash = btnId.substring(0, btnId.lastIndexOf("_"));
+      });
     });
 
     // Update tab content when URL hash changes
     window.addEventListener("hashchange", function () {
-      var hash = window.location.hash.substring(1);
+      const hash = window.location.hash.substring(1);
       if (hash) {
         document.querySelector(".tablinks[id='" + hash + "_btn']").click();
       }
     });
   </script>
+
 
 </html>

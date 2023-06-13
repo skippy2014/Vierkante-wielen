8e80d1ca85b37bd40283bdd7c0c88b877610a3ac
Pjotr <pdevos@roc-dev.com>
2023-06-13 12:10:03 +0200

    Ziekmelden

--- pages/ziekmelden.php
+++ pages/ziekmelden.php
@@ -1,56 +1,111 @@
 <?php
-    session_start();
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/style.css">
-        <title>Vierkanten Wielen</title>
-    </head>
-    <body>
+require_once "../include/db_conn.php";
 
+if (isset($_SESSION['gebruiker'])) {
+  // User is logged in
+  $id_gebruiker = $_SESSION['gebruiker']['id_gebruiker'];
+  echo '<script>console.log("' . $id_gebruiker . '");</script>';
 
-    <h2>Ziekmeldings formulier</h2>
-    <form method="POST" action="">
-        <label for="naam">Naam *:</label>
-        <input type="text" id="naam" name="naam" required><br><br>
+  // Fetch the dates associated with the user from the les table
+  $dateQuery = "SELECT DISTINCT datum_tijd FROM les WHERE id_gebruiker = '$id_gebruiker' AND ziek = ''";
+  $result = mysqli_query($connection, $dateQuery);
+  if (!$result) {
+    die("Query failed: " . mysqli_error($connection));
+  }
 
-        <label for="reden_afwezig">Reden afwezig *:</label>
-        <input type="radio" id="Ziek" name="Reden_Ziek" value="Ziek">
-        <label for="Ziek">Ziek</label>
-        
-        <input type="radio" id="Andere_Reden" name="Reden_Ziek" value="Andere_Reden">
-        <label for="Reden_Ziek">Andere reden</label><br><br>
+  
+  $dates = array();
+  while ($row = mysqli_fetch_assoc($result)) {
+    $dates[] = $row['datum_tijd'];
+  }
 
-        <label for="Telefoon">Telefoon *:</label>
-        <input type="text" id="Telefoon" name="Telefoon" required><br><br>
+  if (isset($_POST['submit'])) {
+    $reden_afmelding = $_POST['reden_afmelding'];
+    $selected_date = $_POST['selected_date'];
 
-        <label for="Contact_Opnemen">Telefonisch contact gewenst? :</label>
-        <input type="radio" id="Ja" name="Contact_Opnemen" value="Ja">
-        <label for="Ja">Ja</label>
-        
-        <input type="radio" id="Nee" name="Contact_Opnemen" value="Nee">
-        <label for="Nee">Nee</label><br><br>
-
-        <label for="Welke_Dag">Welke dag? *:</label>
-        <input type="date" id="Welke_Dag" name="Welke_Dag" required><br><br>
-        
-        <label for="Welke_Dag_Terug">Wanneer weer terug? *:</label>
-        <input type="date" id="Welke_Dag_Terug" name="Welke_Dag_Terug" required><br><br>
-
-
-
-        <input type="submit" value="Verstuur">
     
+    if ($reden_afmelding === "anders") {
+      $anders_reden = $_POST['anders_reden'];
+      
+      
+      $updateOpmerkingQuery = "UPDATE les SET opmerking = '$anders_reden' WHERE id_gebruiker = '$id_gebruiker' AND datum_tijd = '$selected_date'";
+      $updateOpmerkingResult = mysqli_query($connection, $updateOpmerkingQuery);
+      if (!$updateOpmerkingResult) {
+        die("Query failed: " . mysqli_error($connection));
+      }
+    }
+
+    
+    $updateQuery = "UPDATE les SET ziek = 1 WHERE id_gebruiker = '$id_gebruiker' AND datum_tijd = '$selected_date'";
+    $updateResult = mysqli_query($connection, $updateQuery);
+    if (!$updateResult) {
+      die("Query failed: " . mysqli_error($connection));
+    }
+
+    
+    $updateAantalLessenQuery = "UPDATE gebruiker_has_lespakket SET aantallessen = aantallessen + 1 WHERE id_gebruiker = '$id_gebruiker'";
+    $updateAantalLessenResult = mysqli_query($connection, $updateAantalLessenQuery);
+    if (!$updateAantalLessenResult) {
+      die("Query failed: " . mysqli_error($connection));
+    }
+
+    
+    header('location: account_settings.php');
+    exit();
+  }
+} else {
+  header('location: loginpage.php');
+}
+?>
 
 
+<!DOCTYPE html>
+<html lang="en">
 
-    </form>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Vierkanten Wielen</title>
+</head>
 
+<body>
 
+  <h2>Afmelden</h2>
+  <form method="POST" action="">
+    <label for="reden_afmelding">Reden voor afmelding *:</label>
+    <select name="reden_afmelding" id="reden_afmelding" onchange="toggleReasonField()">
+      <option value="ziek">Ziek</option>
+      <option value="anders">Anders</option>
+    </select><br>
 
-    </body>
-    </html>
+    <div id="anders_reason" style="display: none;">
+      <label for="anders_reden">Specificeer de reden:</label>
+      <input type="text" id="anders_reden" name="anders_reden"><br><br>
+    </div>
+
+    <label for="selected_date">Selecteer een datum:</label>
+    <select name="selected_date" id="selected_date">
+      <?php foreach ($dates as $date) : ?>
+        <option value="<?php echo $date; ?>"><?php echo $date; ?></option>
+      <?php endforeach; ?>
+    </select><br><br>
+
+    <button type="submit" name="submit">Verstuur</button>
+  </form>
+
+  <script>
+    function toggleReasonField() {
+      var selectElement = document.getElementById("reden_afmelding");
+      var reasonField = document.getElementById("anders_reason");
+      if (selectElement.value === "anders") {
+        reasonField.style.display = "block";
+      } else {
+        reasonField.style.display = "none";
+      }
+    }
+  </script>
+</body>
+
+</html>

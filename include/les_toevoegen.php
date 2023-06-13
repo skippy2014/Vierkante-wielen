69a3e871bbbca009d4e4556922ff5cd459d41479
Pjotr <pdevos@roc-dev.com>
2023-06-13 12:06:15 +0200

    les toevoegen

--- include/les_toevoegen.php
+++ include/les_toevoegen.php
@@ -16,15 +16,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (mysqli_query($connection, $insertQuery)) {
             $insertedId = mysqli_insert_id($connection);
 
-            $meldingQuery1 = "INSERT INTO melding (id_les, id_gebruiker, bericht, datum_tijd)
-                  VALUES ('$insertedId', '$leerling', 'Een nieuwe les is ingepland.', NOW())";
+            $meldingQuery = "INSERT INTO melding (id_les, id_gebruiker, bericht, datum_tijd)
+                            VALUES ('$insertedId', '$leerling', 'Een nieuwe les is ingepland.', NOW())";
 
-            $meldingQuery2 = "INSERT INTO melding (id_les, id_instructeur, bericht, datum_tijd)
-                  VALUES ('$insertedId', '$instructeur', 'Een nieuwe les is ingepland.', NOW())";
-
-            // Convert idinsttructeur to id_gebruiker with role
-
-            if (mysqli_query($connection, $meldingQuery1) && mysqli_query($connection, $meldingQuery2)) {
+            if (mysqli_query($connection, $meldingQuery)) {
                 $instructeurQuery = "SELECT CONCAT(voornaam, ' ', achternaam) AS instructeur_naam FROM gebruiker WHERE id_gebruiker = '$instructeur'";
                 $instructeurResult = mysqli_query($connection, $instructeurQuery);
 
@@ -32,6 +27,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $instructeurRow = mysqli_fetch_assoc($instructeurResult);
                     $instructeurNaam = $instructeurRow['instructeur_naam'];
                     echo "Les succesvol aangemaakt voor de leerling: " . $instructeurNaam;
+
+                    $updateAantalLessenQuery = "UPDATE gebruiker_has_lespakket SET aantallessen = aantallessen - 1 WHERE id_gebruiker = '$leerling'";
+                    $updateAantalLessenResult = mysqli_query($connection, $updateAantalLessenQuery);
+                    if (!$updateAantalLessenResult) {
+                        echo "Er is een fout opgetreden bij het bijwerken van het aantal lessen: " . mysqli_error($connection);
+                    }
                 } else {
                     echo "Er is een fout opgetreden bij het ophalen van de instructeurgegevens: " . mysqli_error($connection);
                 }
@@ -58,95 +59,87 @@ $lesautoResult = mysqli_query($connection, $lesautoQuery);
 $ingelogdeInstructeurId = $_SESSION["gebruiker"]["id_gebruiker"];
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/style.css">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="../css/style.css">
+            
+            <title>Vierkanten Wielen</title>
+        </head>
 
-        <title>Vierkanten Wielen</title>
-    </head>
-
-    <body>
-        <div class="form-container">
-            <form method="POST" action="">
-                <div class="flex">
-                    <div>
-                        <div class="form-row">
-                            <label for="leerling-select">Selecteer Leerling:</label>
-                            <select id="leerling-select" name="leerling-select" required>
-                                <option value="" disabled selected>-- Selecteer leerling --</option>
-                                <?php
-                                while ($row = mysqli_fetch_assoc($leerlingResult)) {
-                                    echo '<option value="' . $row['id_gebruiker'] . '">' . $row['naam'] . '</option>';
-                                }
-                                ?>
-                            </select>
-                        </div>
-
-                        <div class="form-row">
-                            <label for="instructeur-select">Selecteer Instructeur:</label>
-                            <select id="instructeur-select" name="instructeur-select" required>
-                                <?php
-                                while ($row = mysqli_fetch_assoc($instructeurResult)) {
-                                    $selected = $row['id_gebruiker'] == $ingelogdeInstructeurId ? 'selected' : '';
-                                    echo '<option value="' . $row['id_gebruiker'] . '" ' . $selected . '>' . $row['naam'] . '</option>';
-                                }
-                                ?>
-                            </select>
-                        </div>
-
-                        <div class="form-row">
-                            <label for="auto-select">Selecteer Lesauto:</label>
-                            <select id="auto-select" name="auto-select" required>
-                                <option value="" disabled selected>-- Selecteer auto --</option>
-                                <?php
-                                while ($row = mysqli_fetch_assoc($lesautoResult)) {
-                                    echo '<option value="' . $row['id_lesauto'] . '">' . $row['auto'] . '</option>';
-                                }
-                                ?>
-                            </select>
-                        </div>
+        <body>
+            <div class="form-container">
+                <form method="POST" action="">
+                    <div class="form-row">
+                        <label for="leerling-select">Selecteer Leerling:</label>
+                        <select id="leerling-select" name="leerling-select" required>
+                            <option value="" disabled selected>-- Selecteer leerling --</option>
+                            <?php
+                            while ($row = mysqli_fetch_assoc($leerlingResult)) {
+                                echo '<option value="' . $row['id_gebruiker'] . '">' . $row['naam'] . '</option>';
+                            }
+                            ?>
+                        </select>
                     </div>
-                    <div>
-                        <label for="datum">Selecteer Datum:</label>
-                        <input type="date" id="datum" name="datum" placeholder="Datum" required>
-                        <label for="tijd">Selecteer Tijd:</label>
-                        <input type="time" id="tijd" name="tijd" placeholder="Tijd" required>
-                        <input type="text" id="adres" name="adres" placeholder="Ophaal adres" required>
-                        <input type="text" id="lesdoel" name="lesdoel" placeholder="Lesdoel" required>
+
+                    <div class="form-row">
+                        <label for="instructeur-select">Selecteer Instructeur:</label>
+                        <select id="instructeur-select" name="instructeur-select" required>
+                            <?php
+                            while ($row = mysqli_fetch_assoc($instructeurResult)) {
+                                $selected = $row['id_gebruiker'] == $ingelogdeInstructeurId ? 'selected' : '';
+                                echo '<option value="' . $row['id_gebruiker'] . '" ' . $selected . '>' . $row['naam'] . '</option>';
+                            }
+                            ?>
+                        </select>
                     </div>
-                </div>
-                <button type="submit" value="Verstuur" id="submit-btn">Verstuur</button>
-            </form>
-        </div>
 
-        <script>
-            const leerlingSelect = document.getElementById('leerling-select');
-            const autoSelect = document.getElementById('auto-select');
-            const submitBtn = document.getElementById('submit-btn');
+                    <div class="form-row">
+                        <label for="auto-select">Selecteer Lesauto:</label>
+                        <select id="auto-select" name="auto-select" required>
+                            <option value="" disabled selected>-- Selecteer auto --</option>
+                            <?php
+                            while ($row = mysqli_fetch_assoc($lesautoResult)) {
+                                echo '<option value="' . $row['id_lesauto'] . '">' . $row['auto'] . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <input type="date" id="datum" name="datum" placeholder="Datum" required>
+                    <input type="time" id="tijd" name="tijd" placeholder="Tijd" required>
+                    <input type="text" id="adres" name="adres" placeholder="Ophaal adres" required>
+                    <input type="text" id="lesdoel" name="lesdoel" placeholder="Lesdoel" required>
+                    <button type="submit" value="Verstuur" id="submit-btn">Verstuur</button>
+                </form>
+            </div>
 
-            leerlingSelect.addEventListener('change', validateForm);
-            autoSelect.addEventListener('change', validateForm);
+            <script>
+                const leerlingSelect = document.getElementById('leerling-select');
+                const autoSelect = document.getElementById('auto-select');
+                const submitBtn = document.getElementById('submit-btn');
 
-            function validateForm() {
-                const leerlingValue = leerlingSelect.value;
-                const autoValue = autoSelect.value;
+                leerlingSelect.addEventListener('change', validateForm);
+                autoSelect.addEventListener('change', validateForm);
 
-                if (leerlingValue !== '' && autoValue !== '') {
-                    submitBtn.disabled = false;
-                } else {
-                    submitBtn.disabled = true;
+                function validateForm() {
+                    const leerlingValue = leerlingSelect.value;
+                    const autoValue = autoSelect.value;
+
+                    if (leerlingValue !== '' && autoValue !== '') {
+                        submitBtn.disabled = false;
+                    } else {
+                        submitBtn.disabled = true;
+                    }
                 }
-            }
-        </script>
-    </body>
+            </script>
+        </body>
 
-</html>
+    </html>
 
-<?php
-mysqli_close($connection);
-?>
+    <?php
+    mysqli_close($connection);
+    ?>

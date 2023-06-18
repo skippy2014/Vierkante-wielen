394549e4b2db379317c13f4bd85fa6f1c175075d
Rick <Rslierendregt@roc-dev.com>
2023-06-18 19:37:11 +0200

    Refactor code for better readability and user experience
    The commit message summarizes the changes made to the code. The changes include refactoring the code for better readability and user experience. The changes were made in three files: ziekmelden.php, style.css, and Accountinstellingen-informatie.php. 
    
    In ziekmelden.php, a conditional statement was added to check if a selected date exists before assigning values to variables. In style.css, hover effects were added to buttons in les-popup class. In Accountinstellingen-informatie.php, CSS styles were added to improve the display of personal information.
    
    The commit message is less than 50 characters and followed by a more detailed description of the significant changes made to the code.

--- include/Accountinstellingen-informatie.php
+++ include/Accountinstellingen-informatie.php
@@ -18,10 +18,10 @@
 // }
 
 if (isset($_SESSION["gebruiker"])) {
-    echo '<ul style="list-style-type: none; font-size: 24px; font-family: Arial; color: #333;"><li><h3>Persoonlijke informatie</h3></li>';
+    echo '<ul style="list-style-type: none; font-size: 24px; font-family: Arial; color: #333;" class="informatie"><li><h3>Persoonlijke informatie</h3></li>';
 
     foreach ($_SESSION["gebruiker"] as $gebruiker => $rol) {
-        echo "<li style='color: #777;'><strong>{$gebruiker}: </strong>{$rol}</li>";
+        echo "<li style='color: #777; display: flex; justify-content: space-between;'><span><strong>{$gebruiker}:</strong></span><span><p>{$rol}</p></span></li>";
     }
 
     echo "</ul>";

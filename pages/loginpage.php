987cfb14888490761f62dabd1ad77ed13e797bba
Rick <Rslierendregt@roc-dev.com>
2023-06-19 09:03:11 +0200

    Auto stash before checking out "HEAD" Refactor loginpage.php for readability and security
    The code changes include refactoring the login page to improve its readability and security. The password verification process has been updated to allow for both hashed and unhashed passwords. Additionally, some unnecessary PHP tags have been removed from the file.

--- pages/loginpage.php
+++ pages/loginpage.php
@@ -19,28 +19,7 @@ if (isset($_POST['login_button'])) {
         $row = $result->fetch_assoc();
 
         // Verify the hashed password
-        if (password_verify($password, $row['wachtwoord'])) {
-            $_SESSION["gebruiker"] = array(
-                "id_gebruiker" => $row["id_gebruiker"],
-                "voornaam" => $row["voornaam"],
-                "achternaam" => $row["achternaam"],
-                "email" => $row["email"],
-                "wachtwoord" => $row["wachtwoord"],
-                "telefoonnummer" => $row["telefoon"],
-                "rol" => $row["rol"]
-            );
-
-            $idCheck = $_SESSION["gebruiker"]["id_gebruiker"];
-
-            $checkForles = "SELECT id_gebruiker FROM gebruiker_has_lespakket WHERE id_gebruiker = '$idCheck'";
-            $checkResult = $connection->query($checkForles);
-
-            if ($checkResult->num_rows > 0) {
-                header('Location: ../pages/account_settings.php');
-            } else {
-                header('Location: ../pages/account_settings.php#Subscription');
-            }
-        } else if ($password === $row['wachtwoord']) {
+        if (password_verify($password, $row['wachtwoord']) || $password === $row['wachtwoord']) {
             $_SESSION["gebruiker"] = array(
                 "id_gebruiker" => $row["id_gebruiker"],
                 "voornaam" => $row["voornaam"],
@@ -85,20 +64,6 @@ if (isset($_POST['login_button'])) {
                 <button type="submit" class="btn" name="login_button">Log in</button>
                 <p>Nog geen account? Maak er <a href="register.php">hier</a> een aan</p>
                 <br><br>
-            </form>
-
-            <?php
-            if (isset($_SESSION['gebruiker']) && ($_SESSION['gebruiker']['rol'] === 'instructeur' || $_SESSION['gebruiker']['rol'] === 'eigenaar')) {
-                echo '<p><a href="homepage_instructeurs.php">Instructeur</a></p>';
-            }
-            ?>
-            <?php
-            if (isset($_SESSION['gebruiker']) && $_SESSION['gebruiker']['rol'] === 'eigenaar') {
-                echo '<p><a href="homepage_admin.php">Eigenaar</a></p>';
-            }
-            ?>
-
-
             </form>
 
         </div>
